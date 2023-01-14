cf973a16595fa168ec18a072de89c2422bc8867d
Abha Smriti <smritiabha201099@gmail.com>
2023-01-15 02:53:44 +0530

    Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String [] args){
+        RWOnly r=new RWOnly();
+        r.setName("Shraddha");
+        System.out.println(" Error: you might seen some error, please not it down in commnets");
+        r.getName();
+
+    }
 }
